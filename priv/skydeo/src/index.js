f8261348f3f0abe2183478bf91354f7b953c641b
Peter Olson <peter@overtype.com>
2017-05-18 07:49:14 -0400

    starting react stuff

--- priv/skydeo/src/index.js
+++ priv/skydeo/src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 
 ReactDOM.render(
